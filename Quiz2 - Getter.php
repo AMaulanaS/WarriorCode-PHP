44db13a58a36015e1b159076a15e644ae8d4a9ef
inubayuaji <inubayuaji@gmail.com>
2018-08-10 20:31:25 +0700

    Quiz2

---
+++ Quiz2 - Getter.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @Problem how to get data?
+ * @YourJob make all possibility to get data
+ * @Read https://stackoverflow.com/questions/4478661/getter-and-setter
+ *
+ */
+
+class GetOut
+{
+	private $secret = "This is top secret";
+	public $noSecret = "You can say to everyone";
+
+	/**
+	 * @Hits you can make method here to get something
+	 */
+}
+
+/**
+ * Your code here
+ */
